02af0239cdfea16059f683b80dd97763f2d5072a
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-27 13:56:21 +0530

    with string args

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+class Numbers{
+	public static void main(String []args){
+	for(int Numbers = 0; Numbers <= Numbers.length; Numbers = Numbers+1){
+	System.out.println(Numbers+" ");
+	if(Numbers%2){
+	}else{
+	System.out.print();
+	if(a=1){
+	System.out.println(Numbers+" ");
+}
+}
+}
+}
+}
+	
+	 
---
+++ Xworkz.java
@@ -0,0 +1,7 @@
+class Xworkz{
+	public static void main(String []names){
+	for(int index =0; index <=names.length; index++){
+	System.out.println(names[index]);
+}
+}
+}
